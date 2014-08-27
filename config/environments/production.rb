b19456a3b4652e2c9a7279fb9b4e7f8c655a0270
sharmabloc <sharmaupadhyayula@gmail.com>
2014-08-27 05:26:16 +0000

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Bloccit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # http://sharma-bloccit.herokuapp.com
+  config.action_mailer.default_url_options = {host: 'sharma-bloccit.herokuapp.com'}
 end
