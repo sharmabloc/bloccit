6cad5e74d409257fa4af2f57147684cb2f3aa85d
sharmabloc <sharmaupadhyayula@gmail.com>
2014-08-30 21:42:42 +0000

    added image to posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def update
     if current_user.update_attributes(user_params)
       flash[:notice] = "User information updated"
-      redirect_to_edit_user_registration_path
+      redirect_to_edit_user_registration_path(current_user)
     else
       render "devise/registrations/edit"
     end
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :avatar)
   end
 end
